41561842524cfe653881ad878fa270a203ecf4e8
tiiicup555 <dilnazmamutova1@gmail.com>
2024-09-26 23:28:52 +0600

    not all h/w

---
+++ src/beec-1028.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+class Java1 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int count = in.nextInt();
+
+        for (int i = 0; i < count; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();432`
+
+            if (a < b) {
+                int t = a;
+                a = b;
+                b = t;
+            }
+
+            while (b != 0) {
+                int t = a;
+                a = b;
+                b = t % b;
+            }
+
+            System.out.println(a);
+        }
+
+        in.close();
+    }
+}
